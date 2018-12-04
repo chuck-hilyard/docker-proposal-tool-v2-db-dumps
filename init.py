f95df351d6d93818d06101366e952ebd904d0362
Chuck Hilyard <chuck.hilyard@reachlocal.com>
2018-12-04 09:30:02 -0800

    added s3 sync

--- init.py
+++ init.py
@@ -1,3 +1,4 @@
+# environment variables are set via terraform
 
 import os
 import subprocess
@@ -6,7 +7,7 @@ import datetime
 
 
 def take_dump():
-  # these environment variables are set via terraform
+  print("mongodump")
   MONGO_INITDB_ROOT_USERNAME = os.environ['MONGO_INITDB_ROOT_USERNAME']
   MONGO_INITDB_ROOT_PASSWORD = os.environ['MONGO_INITDB_ROOT_PASSWORD']
   datestring = datetime.datetime.today().strftime('%Y-%m-%d-%H%M')
@@ -16,11 +17,18 @@ def take_dump():
   target_dir = "/data/backups/{}".format(datestring)
   subprocess.run(["mongodump", "-u", MONGO_INITDB_ROOT_USERNAME, "-p", MONGO_INITDB_ROOT_PASSWORD, "-h", target_host, "-o", target_dir])
 
+def sync_dumps_to_s3():
+  print("syncing to s3")
+  target_bucket = "s3://proposal-tool-v2-db-{}-{}/backups".format(os.environ['environment'], os.environ['platform'])
+  subprocess.run(["aws", "s3", "sync", "/data/backups", target_bucket])
 
 def main():
   while True:
     print("main loop")
     take_dump()
+    time.sleep(2)
+    sync_dumps_to_s3()
+    print("sleeping for 2 hours")
     time.sleep(3600)
 
 if __name__ == '__main__':
